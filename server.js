7d2b9d7123afa3d87cd9b43f7dd790884dcf9b3b
sukuna <ayoub7math@gmail.com>
2024-06-03 14:34:15 +0200

    final version

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const path = require("path");
-
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const server = require("http").createServer(app);
-
 const io = require("socket.io")(server);
-
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100, 
+    message: 'Trop de requêtes de cette IP, veuillez réessayer après 15 minutes'
+});
+app.use(limiter);
 app.use(express.static(path.join(__dirname + "/public")));
+app.use(helmet());
 app.get("/receiver.html", (req, res) => {
     res.sendFile(__dirname + "/public/receiver.html");
 });
